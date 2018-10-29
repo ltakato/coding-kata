dea35161f0a221a4765f0deda0fca70d5c0d778f
leonardo takato <ltakato@vntrx.com>
2018-10-29 15:23:56 -0300

    fix: fix typos for fizz buzz

--- 01-fizzbuzz/fizzbuzz.js
+++ 01-fizzbuzz/fizzbuzz.js
@@ -1,8 +1,8 @@
-function fuzzyBuzz(number) {
+function fizzyBuzz(number) {
     if (number % 3 === 0 && number % 5 === 0) return 'fizzbuzz';
     if (number % 3 === 0) return 'fizz';
     if (number % 5 === 0) return 'buzz';
     return number;
 }
 
-module.exports = fuzzyBuzz; 
+module.exports = fizzyBuzz; 
--- 01-fizzbuzz/fizzbuzz.spec.js
+++ 01-fizzbuzz/fizzbuzz.spec.js
@@ -1,17 +1,17 @@
-const fizzybuzz = require('./fizzbuzz');
+const fizzbuzz = require('./fizzbuzz');
 
 test('it should return the number if not divisible by 3 or 5', () => {
-    expect(fizzybuzz(1)).toBe(1);
+    expect(fizzbuzz(1)).toBe(1);
 });
 
 test('it should return \'fizz\' if divisible 3', () => {
-    expect(fizzybuzz(3)).toBe('fizz');
+    expect(fizzbuzz(3)).toBe('fizz');
 });
 
 test('it should return \'buzz\' if divisible 5', () => {
-    expect(fizzybuzz(5)).toBe('buzz');
+    expect(fizzbuzz(5)).toBe('buzz');
 });
 
 test('it should return \'fizzbuzz\' if divisible 3 and 5', () => {
-    expect(fizzybuzz(15)).toBe('fizzbuzz');
+    expect(fizzbuzz(15)).toBe('fizzbuzz');
 });
