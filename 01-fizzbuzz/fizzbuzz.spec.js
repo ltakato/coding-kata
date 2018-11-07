6d7da0d07a7eb8eb8feaa3482781cde43f9e6017
leonardo takato <ltakato@vntrx.com>
2018-11-07 12:45:28 -0200

    fix: wrap test cases on describe

--- 01-fizzbuzz/fizzbuzz.spec.js
+++ 01-fizzbuzz/fizzbuzz.spec.js
@@ -1,17 +1,19 @@
 const fizzbuzz = require('./fizzbuzz');
 
-test('it should return the number if not divisible by 3 or 5', () => {
-    expect(fizzbuzz(1)).toBe(1);
-});
+describe('kata 01 - fizzbuzz', () => {
+    it('it should return the number if not divisible by 3 or 5', () => {
+        expect(fizzbuzz(1)).toBe(1);
+    });
 
-test('it should return \'fizz\' if divisible 3', () => {
-    expect(fizzbuzz(3)).toBe('fizz');
-});
+    it('it should return \'fizz\' if divisible 3', () => {
+        expect(fizzbuzz(3)).toBe('fizz');
+    });
 
-test('it should return \'buzz\' if divisible 5', () => {
-    expect(fizzbuzz(5)).toBe('buzz');
-});
+    it('it should return \'buzz\' if divisible 5', () => {
+        expect(fizzbuzz(5)).toBe('buzz');
+    });
 
-test('it should return \'fizzbuzz\' if divisible 3 and 5', () => {
-    expect(fizzbuzz(15)).toBe('fizzbuzz');
+    it('it should return \'fizzbuzz\' if divisible 3 and 5', () => {
+        expect(fizzbuzz(15)).toBe('fizzbuzz');
+    });
 });
