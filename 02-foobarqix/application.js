e1a14dffb5d4bdbcf57c0bd2e765ce834e12d75c
takato <ltakato@hotmail.com>
2018-11-27 12:17:31 -0200

    fix: fix application presentation

--- 02-foobarqix/application.js
+++ 02-foobarqix/application.js
@@ -1,5 +1,5 @@
 const { foobarqix }= require('./foobarqix');
 
 for (let number = 1; number <=100; number++) {
-    console.log(`${number}: ${foobarqix(number)}`);
+    console.log(`${number} => ${foobarqix(number)}`);
 }
